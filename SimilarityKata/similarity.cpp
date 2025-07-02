707336a9d4afbc655035adab9869561a5104ce90
Hyunseok <lukiaeve@gmail.com>
2025-07-02 16:23:23 +0900

    [feature] applied Teachr code

--- SimilarityKata/similarity.cpp
+++ SimilarityKata/similarity.cpp
@@ -1,17 +1,37 @@
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using std::string;
+using std::vector;
 
-class Simil {
+class LengthChecker {
 public:
-    int lengthCheckScore(string str1, string str2) {
-        if (str1.length() == str2.length()) {
-            score = 60;
-        }
+    int getResult(string input1, string input2) {
 
-        return score;
+        int A = std::max(input1.length(), input2.length());
+        int B = std::min(input1.length(), input2.length());
+
+        if (isSameLength(A, B)) return MAX_LENGTH_POINT;
+        if (isDoubleLength(A, B)) return MIN_LENGTH_POINT;
+        return getPartialLengthPoint(A, B);
     }
 
 private:
-    int score;
+    int getPartialLengthPoint(int A, int B) {
+        int Gap = A - B;
+        return MAX_LENGTH_POINT - MAX_LENGTH_POINT * Gap / B;
+    }
+
+    bool isSameLength(int A, int B) {
+        return A == B;
+    }
+
+    bool isDoubleLength(int A, int B) {
+        return A >= B * 2;
+    }
+
+    static const int MAX_LENGTH_POINT = 60;
+    static const int MIN_LENGTH_POINT = 0;
+
 };
--- SimilarityKata/similarity_test.cpp
+++ SimilarityKata/similarity_test.cpp
@@ -3,16 +3,34 @@
 
 using namespace testing;
 
-class SimilFixture : public Test {
+class LengthFixture : public Test {
 public:
-    Simil sml;
-    void lenChk(int expected, string str1, string str2) {
-        int ret = sml.lengthCheckScore(str1, str2);
+    void diag(int expected, string input1, string input2) {
+        int ret = app.getResult(input1, input2);
         EXPECT_EQ(expected, ret);
     }
+
+    LengthChecker app;
 };
 
-TEST_F(SimilFixture, LenChk1) {
-    lenChk(60, "ASD", "DSA");
+TEST_F(LengthFixture, TC1) {
+    diag(60, "ABC", "UEI");
 }
+
+TEST_F(LengthFixture, TC2) {
+    diag(0, "WERABC", "UEI");
+}
+
+TEST_F(LengthFixture, TC3) {
+    diag(0, "WERBC", "UEWERREWWERI");
+}
+
+TEST_F(LengthFixture, TC4) {
+    diag(20, "WERBC", "EWD");
+}
+
+TEST_F(LengthFixture, TC5) {
+    diag(20, "WER", "EREWD");
+}
+
 
